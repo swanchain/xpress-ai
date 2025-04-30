84f5cbeffe2c0a2060bf98f4fea48ee9cf6f8391
rykci <50771299+rykci@users.noreply.github.com>
2025-04-30 16:14:18 -0400

    implement refresh my vibe button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,6 +10,7 @@ import RobotIcon from "./RobotIcon";
 import LogoutIcon from "./LogoutIcon";
 import apiClient from "@/services/apiClient";
 import CloseIcon from "./CloseIcon";
+import Spinner from "./Spinner";
 
 export default function Navbar({
   user,
@@ -31,6 +32,8 @@ export default function Navbar({
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isAiInfoOpen, setIsAiInfoOpen] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [refreshError, setRefreshError] = useState("");
   const [aiDetails, setAiDetails] = useState({});
 
   // useEffect(() => {}, [])
@@ -42,8 +45,26 @@ export default function Navbar({
 
     setAiDetails({ ...details, parsed_personality_traits: parsed.traits });
 
+    setRefreshError("");
     setIsDropdownOpen(false);
     setIsAiInfoOpen(true);
+  };
+
+  const refreshMyVibe = async () => {
+    setRefreshError("");
+    setIsRefreshing(true);
+    try {
+      const response = await apiClient.post("/ai-vibe/refresh-my-vibe");
+      const details = response.data;
+      const parsed = JSON.parse(details.personality_traits);
+
+      setAiDetails({ ...details, parsed_personality_traits: parsed.traits });
+    } catch (err) {
+      console.log("err", err);
+      setRefreshError(err.response?.data?.detail || err.message || "");
+    } finally {
+      setIsRefreshing(false);
+    }
   };
 
   return (
@@ -186,10 +207,14 @@ export default function Navbar({
                 <div className="w-full flex flex-row flex-wrap gap-2">
                   {aiDetails.parsed_personality_traits.map((trait, i) => (
                     <div
-                      className="rounded-full p-2 bg-black/5 w-fit text-sm"
+                      className={`rounded-full p-2 bg-black/5 w-fit text-sm ${
+                        isRefreshing ? "animate-pulse" : ""
+                      }`}
                       key={i}
                     >
-                      {trait}
+                      <span className={isRefreshing ? "opacity-0" : ""}>
+                        {trait}
+                      </span>
                     </div>
                   ))}
                 </div>
@@ -201,21 +226,57 @@ export default function Navbar({
 
           <div>
             <h1 className="font-semibold text-xl">Background Story</h1>
-            <p className="py-2">{aiDetails.background_story || ""}</p>
+            {isRefreshing ? (
+              <div className="flex animate-pulse space-x-4 py-2">
+                <div className="flex-1 space-y-6 py-1">
+                  <div className="space-y-3">
+                    <div className="grid grid-cols-3 gap-4">
+                      <div className="col-span-2 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-1 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-2 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-1 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-1 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-2 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-1 h-2 rounded bg-black/5"></div>
+                      <div className="col-span-2 h-2 rounded bg-black/5"></div>
+                    </div>
+                    <div className="h-2 rounded bg-black/5"></div>
+                    <div className="h-2 rounded bg-black/5"></div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <p className="py-2">{aiDetails.background_story || ""}</p>
+            )}
           </div>
 
           <div>
             <h1 className="font-semibold text-xl">Category</h1>
-            <p className="py-2">{aiDetails.category || ""}</p>
-          </div>
-
-          <div>
-            <h1 className="font-semibold text-xl">Language</h1>
-            <p className="py-2">{aiDetails.language || ""}</p>
+            {isRefreshing ? (
+              <div className="flex animate-pulse space-x-4 py-2">
+                <div className="flex-1 space-y-6 py-1">
+                  <div className="h-2 rounded bg-black/5"></div>
+                </div>
+              </div>
+            ) : (
+              <p className="py-2">{aiDetails.category || ""}</p>
+            )}
           </div>
 
           <div className="flex items-center justify-end">
-            <button className="black-btn">Refresh My Vibe</button>
+            <span className="flex-1 text-sm text-red-600">{refreshError}</span>
+            {isRefreshing ? (
+              <button className="black-btn hover:">
+                <div className="flex flex-row items-center gap-2">
+                  <Spinner />{" "}
+                  <span className="animate-pulse">Refreshing...</span>
+                </div>
+              </button>
+            ) : (
+              <button onClick={refreshMyVibe} className="black-btn">
+                Refresh My Vibe
+              </button>
+            )}
           </div>
         </div>
       </div>
